4dab70a04a5fc27f163dcc9e5947edeb3cbfbc3a
yuncoffee <dong072815@gmail.com>
2024-11-03 17:25:45 +0900

    [chore] font system test

--- src/components/core/text/text.styles.ts
+++ src/components/core/text/text.styles.ts
@@ -3,5 +3,16 @@ import { css } from "lit"
 export const textStyles = css`
     :host {
         display: inline-flex;
+        font-size: var(--font-size, 16px);
+        font-weight: var(--font-weight, 500);
+        line-height: var(--line-height, 1.5em);
+        letter-spacing: var(--letter-spacing, 0);
+    }
+
+    :host(h2) {
+        font-size: inherit;
+        font-weight: inherit;
+        line-height: inherit;
+        letter-spacing: inherit;
     }
 `
